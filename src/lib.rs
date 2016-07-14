c3259bd9f03a0d605c3684231831d500f7562f8c
Zach Bray <zachbray@googlemail.com>
2016-07-14 01:04:59 +0100

    Added more of the raft state machine.

--- src/lib.rs
+++ src/lib.rs
@@ -16,24 +16,42 @@ struct LogEntry {
     data: [u8]
 }
 
-enum Message<'a> {
-    RequestVoteRequest { 
-        last_log_term: Term, 
-        last_log_index: LogIndex 
-    },
-
-    AppendEntriesRequest {
-        prev_index: LogIndex,
-        prev_term: Term,
-        entries: &'a [&'a LogEntry],
-        commit_index: LogIndex
-    }
+struct RequestVoteRequest {
+    last_log_term: Term, 
+    last_log_index: LogIndex 
 }
 
-struct Envelope<'a> {
+struct RequestVoteReply {
+    granted: bool
+}
+
+struct AppendEntriesRequest<'a> {
+    prev_index: LogIndex,
+    prev_term: Term,
+    entries: &'a [&'a LogEntry],
+    commit_index: LogIndex
+}
+
+struct AppendEntriesReply {
+    success: bool,
+    match_index: LogIndex
+}
+
+enum Message<'a> {
+    RequestVoteRequest(RequestVoteRequest),
+    RequestVoteReply(RequestVoteReply),
+    AppendEntriesRequest(AppendEntriesRequest<'a>),
+    AppendEntriesReply(AppendEntriesReply)
+}
+
+struct Header {
     from: ServerId,
     to: ServerId,
     term: Term,
+}
+
+struct Envelope<'a> {
+    header: Header,
     message: Message<'a>
 }
 
@@ -76,9 +94,8 @@ impl Alarm {
         }
     }
 
-    fn is_due<TModel>(&self, model: &TModel) -> bool 
-    where TModel: Model {
-        self.due_time <= model.time()
+    fn is_due(&self, time: Duration) -> bool {
+        self.due_time <= time
     }
 }
 
@@ -91,6 +108,16 @@ struct Peer {
 }
 
 impl Peer {
+    fn new() -> Self {
+        Peer {
+            vote_granted: false,
+            match_index: 0,
+            next_index: 1,
+            rpc_alarm: Alarm::due_now(),
+            heartbeat_alarm: Alarm::due_now()
+        }
+    }
+
     fn reset(&mut self) {
         self.vote_granted = false;
         self.match_index = 0;
@@ -108,9 +135,11 @@ trait Log {
     fn term_at(&self, index: LogIndex) -> Term;
     fn length(&self) -> LogIndex;
     fn entries<'a>(&'a self, from_index_incl: LogIndex, until_index_excl: LogIndex) -> &'a[&'a LogEntry];
+    fn truncate(&mut self, to_index_incl: LogIndex);
+    fn append(&mut self, entry: &LogEntry);
 }
 
-struct Server<TLog> where TLog: Log {
+struct Server<TLog, TModel> where TLog: Log, TModel: Model {
     id: ServerId,
     term: Term,
     commit_index: LogIndex,
@@ -118,23 +147,41 @@ struct Server<TLog> where TLog: Log {
     voted_for: Option<ServerId>,
     election_alarm: Alarm,
     peers: HashMap<ServerId, Peer>,
-    log: TLog
+    log: TLog,
+    model: TModel
 }
 
-impl <TLog> Server<TLog> where TLog: Log {
-    fn step_down<TModel>(&mut self, model: &mut TModel, term: u64) where TModel: Model {
-        self.term = term; 
-        self.state = ServerState::Follower;
-        self.voted_for = Option::None;
-        if self.election_alarm.is_due(model) {
-            self.election_alarm = Alarm::new(model.random_election_timeout());
+impl <TLog, TModel> Server<TLog, TModel> where TLog: Log, TModel: Model {
+    fn new(id: ServerId, peers: &[ServerId], log: TLog, model: TModel) -> Self {
+        Server {
+            id: id,
+            peers: peers.iter().fold(HashMap::new(), |mut acc, &p| {
+                acc.insert(p, Peer::new());
+                acc
+            }),
+            state: ServerState::Follower,
+            term: 1,
+            voted_for: Option::None,
+            log: log,
+            model: model,
+            commit_index: 0,
+            election_alarm: Alarm::due_now()
         }
     }
 
-    fn start_new_election<TModel>(&mut self, model: &mut TModel) where TModel: Model {
+    fn step_down(&mut self, term: u64) {
+        self.term = term; 
+        self.state = ServerState::Follower;
+        self.voted_for = Option::None;
+        if self.election_alarm.is_due(self.model.time()) {
+            self.election_alarm = Alarm::new(self.model.random_election_timeout());
+        }
+    }
+
+    fn start_new_election(&mut self) {
         match self.state {
-            ServerState::Follower | ServerState::Candidate if self.election_alarm.is_due(model) => {
-                self.election_alarm = Alarm::new(model.random_election_timeout());
+            ServerState::Follower | ServerState::Candidate if self.election_alarm.is_due(self.model.time()) => {
+                self.election_alarm = Alarm::new(self.model.random_election_timeout());
                 self.term += 1;
                 self.voted_for = Option::Some(self.id);
                 self.state = ServerState::Candidate;
@@ -146,21 +193,23 @@ impl <TLog> Server<TLog> where TLog: Log {
         }
     }
 
-    fn send_request_vote<TModel>(&mut self, model: &mut TModel, peer_id: &ServerId) where TModel: Model {
+    fn send_request_vote(&mut self, peer_id: &ServerId) {
         match self.state {
             ServerState::Candidate => {
                 match self.peers.get_mut(peer_id) {
                     Option::Some(peer) => {
-                        if peer.rpc_alarm.is_due(model) {
-                            peer.rpc_alarm = Alarm::new(model.time() + model.rpc_timeout());
-                            model.send(Envelope {
-                                from: self.id,
-                                to: peer_id.clone(),
-                                term: self.term,
-                                message: Message::RequestVoteRequest {
+                        if peer.rpc_alarm.is_due(self.model.time()) {
+                            peer.rpc_alarm = Alarm::new(self.model.time() + self.model.rpc_timeout());
+                            self.model.send(Envelope {
+                                header: Header {
+                                    from: self.id,
+                                    to: peer_id.clone(),
+                                    term: self.term
+                                },
+                                message: Message::RequestVoteRequest(RequestVoteRequest {
                                     last_log_term: self.log.term(),
                                     last_log_index: self.log.length()
-                                }
+                                })
                             });
                         }
                     },
@@ -189,33 +238,35 @@ impl <TLog> Server<TLog> where TLog: Log {
         }
     }
 
-    fn send_append_entries<TModel>(&mut self, model: &mut TModel, peer_id: &ServerId) where TModel: Model {
+    fn send_append_entries(&mut self, peer_id: &ServerId) {
         match self.peers.get_mut(peer_id) {
             Option::Some(peer) =>
                 match self.state {
                     ServerState::Leader if 
-                        peer.heartbeat_alarm.is_due(model)
-                        || (peer.rpc_alarm.is_due(model) && peer.next_index <= self.log.length()) => {
+                        peer.heartbeat_alarm.is_due(self.model.time())
+                        || (peer.rpc_alarm.is_due(self.model.time()) && peer.next_index <= self.log.length()) => {
                         let prev_index = peer.next_index - 1;
                         let last_index = 
                             if peer.match_index + 1 < peer.next_index {
                                 prev_index
                             } else {
-                                std::cmp::min(prev_index + model.batch_size(), self.log.length())
+                                std::cmp::min(prev_index + self.model.batch_size(), self.log.length())
                             };
-                        model.send(Envelope {
-                            from: self.id,
-                            to: peer_id.clone(),
-                            term: self.term,
-                            message: Message::AppendEntriesRequest {
+                        self.model.send(Envelope {
+                            header: Header {
+                                from: self.id,
+                                to: peer_id.clone(),
+                                term: self.term
+                            },
+                            message: Message::AppendEntriesRequest(AppendEntriesRequest {
                                 prev_index: prev_index,
                                 prev_term: self.log.term_at(prev_index),
                                 entries: self.log.entries(prev_index, last_index),
                                 commit_index: std::cmp::min(self.commit_index, last_index)
-                            }
+                            })
                         });
-                        peer.rpc_alarm = Alarm::new(model.time() + model.rpc_timeout());
-                        peer.heartbeat_alarm = Alarm::new(model.time() + model.max_election_timeout() / 2)
+                        peer.rpc_alarm = Alarm::new(self.model.time() + self.model.rpc_timeout());
+                        peer.heartbeat_alarm = Alarm::new(self.model.time() + self.model.max_election_timeout() / 2)
                     },
                     _ => {}
                 },
@@ -241,6 +292,128 @@ impl <TLog> Server<TLog> where TLog: Log {
                 }
             },
             _ => {}
+        }
+    }
+
+    fn handle_request_vote_request(&mut self, header: &Header, message: &RequestVoteRequest) {
+        if self.term < header.term {
+            self.step_down(header.term);
+        }
+        let has_same_term = self.term == header.term;
+        let this_server_did_not_vote = self.voted_for.is_none();
+        let this_server_did_vote_for_them = match self.voted_for { 
+            Option::Some(peer_id) if peer_id == header.from => true,
+            _ => false 
+        };
+        let log_term = self.log.term();
+        let that_server_has_at_least_as_many_entries = 
+            message.last_log_term > log_term || 
+            (message.last_log_term == log_term && message.last_log_index >= self.log.length());
+        let granted =
+            has_same_term && 
+            (this_server_did_not_vote || this_server_did_vote_for_them) &&
+            that_server_has_at_least_as_many_entries;
+        if granted {
+            self.voted_for = Option::Some(header.from);
+            self.election_alarm = Alarm::new(self.model.random_election_timeout());
+        }
+        self.model.send(Envelope {
+            header: Header {
+                to: header.from,
+                from: self.id,
+                term: self.term
+            },
+            message: Message::RequestVoteReply(RequestVoteReply {
+                granted: granted
+            })
+        });
+    }
+
+    fn handle_request_vote_reply(&mut self, header: &Header, message: &RequestVoteReply) {
+        match self.state {
+            ServerState::Candidate if self.term == header.term => {
+                match self.peers.get_mut(&header.from) {
+                    Option::Some(peer) => {
+                        peer.rpc_alarm = Alarm::due_never();
+                        peer.vote_granted = message.granted;
+                    },
+                    _ => {}
+                }
+            },
+            _ if self.term < header.term => {
+                self.step_down(header.term);
+            },
+            _ => {}
+        }
+    }
+
+    fn handle_append_entries_request(&mut self, header: &Header, message: &AppendEntriesRequest) {
+        let mut success = false;
+        let mut match_index = 0;
+        match header.term {
+            t if t > self.term => self.step_down(t),
+            t if t == self.term => {
+                self.state = ServerState::Follower;
+                self.election_alarm = Alarm::new(self.model.random_election_timeout());
+                if message.prev_index == 0 || (
+                    message.prev_index <= self.log.length() && 
+                    self.log.term_at(message.prev_index) == message.prev_term) {
+                    success = true;
+                    match_index = message.prev_index;
+                    for entry in message.entries {
+                        match_index += 1;
+                        if self.log.term_at(match_index) != entry.term {
+                            self.log.truncate(match_index);
+                            self.log.append(entry);
+                        }
+                    }
+                    self.commit_index = std::cmp::max(self.commit_index, message.commit_index);
+                }
+            },
+            _ => {}
+        }
+        self.model.send(Envelope {
+            header: Header {
+                from: self.id,
+                to: header.from,
+                term: self.term
+            },
+            message: Message::AppendEntriesReply(AppendEntriesReply {
+                success: success,
+                match_index: match_index
+            })
+        });
+    }
+
+    fn handle_append_entries_reply(&mut self, header: &Header, message: &AppendEntriesReply) {
+        match self.state {
+            ServerState::Leader if self.term == header.term => {
+                match self.peers.get_mut(&header.from) {
+                    Option::Some(peer) => {
+                        if message.success {
+                            peer.match_index = std::cmp::max(peer.match_index, message.match_index);
+                            peer.next_index = peer.match_index + 1;
+                        } else {
+                            peer.next_index = std::cmp::max(1, peer.next_index - 1);
+                        }
+                        peer.rpc_alarm = Alarm::due_now();
+                    },
+                    _ => {}
+                }
+            },
+            _ if self.term < header.term => {
+                self.step_down(header.term);
+            },
+            _ => {}
+        }
+    }
+
+    fn handle_message(&mut self, envelope: &Envelope) {
+        match envelope.message {
+            Message::RequestVoteRequest(ref message) => self.handle_request_vote_request(&envelope.header, message),
+            Message::RequestVoteReply(ref message) => self.handle_request_vote_reply(&envelope.header, message),
+            Message::AppendEntriesRequest(ref message) => self.handle_append_entries_request(&envelope.header, message),
+            Message::AppendEntriesReply(ref message) => self.handle_append_entries_reply(&envelope.header, message)
         }
     }
 }
